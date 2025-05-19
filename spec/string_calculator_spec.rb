270f67abbe7d3d637c2dbc44e7c90de8711ec2a0
its-mr-raghav <raghavrajsharma112@gmail.com>
2025-05-19 16:17:33 +0530

    Supports newline character as delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,8 +2,8 @@ require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
   describe '.add' do
-    it 'returns sum of multiple comma-separated numbers' do
-        expect(StringCalculator.add("1,2,3,4")).to eq(10)
-    end  
+    it 'handles new lines as valid delimiters' do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end      
   end
 end
